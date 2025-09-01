1a4ad9d1ad8fe71f1a442045826881bdd86afb3b
ShawnSomething <khooweishawn@gmail.com>
2025-09-02 07:39:01 +1000

    updated quests

--- sqs/backend/main.ts
+++ sqs/backend/main.ts
@@ -1,19 +1,3 @@
-// 1. start session with introductory things
-// 2. a random quest is chosen for you
-// 3. ability to reject quest
-// 4. character gets sad
-// 5. another random quest is chosen - not the same as the one that was just given to you
-// 6. ability to accept quest
-// 7. character happy when you accepted quest
-// 8. complete quest - character happy
-// 9. gives next quest in the hierarchy
-// 10. repeat until no quests left
-// 11. give another quests?
-// 12. failed quest - character sad
-// 13. repeat 11
-// 14. yes - randomly generate another quest - not the same as the one just given
-// 15. no - character sad and waiting for your return
-// 16. upon return - repeat 11.
 
 import * as fs from "fs";
 import path from "path";
@@ -31,16 +15,43 @@ type Quest = {
     } []
 }
 
+const start = () => {
+    console.log("Hey there! I am trapped in your device! help me experience the world by doing these quests~")
+    console.log(questSelect())
+}
+
 const questSelect = () => {
     const selectedId = Math.floor(Math.random() * 10)
     const selectedQuest = quests.find((q: { id: number; }) => q.id === selectedId)
 
     if (selectedQuest) {
-        console.log(selectedId, selectedQuest.quest) 
+        return { selectedId, quest: selectedQuest.quest }
+    }
+
+    acceptQuest(true)
+}
+
+const acceptQuest = (questAccepted: boolean) => {
+    if (questAccepted == true) {
+        console.log("Yippie! Let me know when you are done!")
+        completeQuest(true)
+    }
+    else if (questAccepted == false) {
+        console.log("Aw, ok, let's try another one")
+        questSelect()
     }
 }
 
-questSelect()
+const subQuestSelect = () => {
+    const selectedSubId = Math.random() * 10
+    const selectedSubQuest = quests.find((q: { id: number; }) => q.id === selectedSubId)
+}
+
+const completeQuest = (questComplete: boolean) => {
+
+}
+
+start()
 
 
 
