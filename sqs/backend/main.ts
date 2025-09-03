5074a64caf492657bdecaf9ab9c26042ef49b880
ShawnSomething <khooweishawn@gmail.com>
2025-09-04 07:02:32 +1000

    console select

--- sqs/backend/main.ts
+++ sqs/backend/main.ts
@@ -1,6 +1,7 @@
 
 import * as fs from "fs";
 import path from "path";
+import inquirer from "inquirer"
 
 const questsPath = path.join(__dirname, "quests.json"); 
 const quests = JSON.parse(fs.readFileSync(questsPath, "utf-8"));
@@ -8,19 +9,26 @@ const quests = JSON.parse(fs.readFileSync(questsPath, "utf-8"));
 let selectedQuest: { id: number; quest: string; followUps: any[] } | null = null;
 
 const start = () => {
-    console.log("Hey there! I am trapped in your device! help me experience the world by doing these quests~")
+    console.log("Hey there! I am trapped in your device! help me experience the wonders of life by doing these quests~")
     console.log(questSelect())
 }
 
-const questSelect = () => {
+const questSelect = async () => {
     const selectedId = Math.floor(Math.random() * quests.length)
     selectedQuest = quests.find((q: { id: number; }) => q.id === selectedId)
 
-    if (selectedQuest) {
-        return { id: selectedQuest.id , quest: selectedQuest.quest, followUps: selectedQuest.followUps }
-    }
+    console.log ({ id: selectedQuest?.id , quest: selectedQuest?.quest})
     
-    acceptQuest(true)
+    const answer = await inquirer.prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "Will you help me experience this?",
+            choices: ["Complete", "Reject"],
+        },
+    ])
+
+    await completeQuest(answer.choice)
 }
 
 const subQuestSelect0 = () => {
@@ -41,28 +49,18 @@ const subQuestSelect1 = () => {
     console.log ({ parentId: selectedQuest.id, subQuest: selectedSubQuest})
 }
 
-const acceptQuest = (questAccepted: boolean) => {
-    if (questAccepted == true) {
-        console.log("Yippie! Let me know when you are done!")
-        completeQuest(true)
-    }
-    else if (questAccepted == false) {
-        console.log("Aw, ok, let's try another one")
-        questSelect()
-    }
-}
-
-const completeQuest = (questComplete: boolean) => {
-    if (questComplete == true) {
+const completeQuest = (choice: string) => {
+    if (choice === "Complete") {
         subQuestSelect0()
     }
-    else if (questComplete == false) {
-        console.log("Ok, I'll wait. Or did you want to start a brand new quest?")
+    else if (choice == "Reject") {
+        console.log("Aw... Did you want to start a brand new quest?")
         questSelect()
     }
 }
 
 start()
+
 
 
 
