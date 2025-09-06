6af21696baf3792260c332f4d99916e1a967c5a1
ShawnSomething <khooweishawn@gmail.com>
2025-09-06 12:17:52 +1000

    finally the ts config stopped erroring

--- sqs/backend/server.ts
+++ sqs/backend/server.ts
@@ -5,7 +5,7 @@ import {start, completeQuest} from "./main"
 const app = express()
 const PORT = 4000
 app.use(cors())
-app.use(express.json)
+app.use(express.json())
 
 app.post("/start", (req, res) => {
     const startQuest = start()
