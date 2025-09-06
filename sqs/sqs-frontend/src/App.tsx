9847f913a85bed1ae48783303b9e9e4f753db356
ShawnSomething <khooweishawn@gmail.com>
2025-09-06 16:50:45 +1000

    load hook up to /start

--- sqs/sqs-frontend/src/App.tsx
+++ sqs/sqs-frontend/src/App.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [questStep, setQuestStep] = useState<string>("")
+
+  useEffect(() => {
+    fetch("http://localhost:4000/start", {method: "POST"})
+      .then(res => res.json())
+      .then(data => setQuestStep(data.step))
+      .catch(err => console.error("failed initial load", err))
+  })
+
   return (
     <>
       <div className='header'>
         <h1>Side Quest Simulator</h1>
       </div>
-      <div className='body'></div>
+      <div className='body'>{questStep}</div>
       <div className='guy'></div>
       <div className="buttons">
         <button className="buttons complete">Complete</button>
