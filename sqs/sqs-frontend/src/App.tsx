48d2a021c326cded5d099f0cb246f02fbd5f7d38
ShawnSomething <khooweishawn@gmail.com>
2025-09-06 12:52:59 +1000

    who needs tailwind anyways

---
+++ sqs/sqs-frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='header'>
+        <h1>Side Quest Simulator</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
